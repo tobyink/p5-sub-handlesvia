960017f128b4941edd869a80776e2ad5a588074e
Toby Inkster <mail@tobyinkster.co.uk>
2020-01-26 21:20:42 +0000

    perl-reversion was being annoying

--- lib/Sub/HandlesVia.pm
+++ lib/Sub/HandlesVia.pm
@@ -7,7 +7,7 @@ package Sub::HandlesVia;
 use Exporter::Shiny qw( delegations );
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.008';
+our $VERSION   = '0.008_000';
 
 sub _generate_delegations {
 	my ($me, $name, $args, $globals) = (shift, @_);
--- lib/Sub/HandlesVia/HandlerLibrary.pm
+++ lib/Sub/HandlesVia/HandlerLibrary.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::HandlerLibrary;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.008';
+our $VERSION   = '0.008_000';
 
 use Types::Standard qw( Any Item );
 
