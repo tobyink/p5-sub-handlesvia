d64f2d7cf4b749edf30f3b3fdae28d79011c3976
Toby Inkster <mail@tobyinkster.co.uk>
2020-09-12 15:07:00 +0100

    bump version; changelog

--- lib/Sub/HandlesVia.pm
+++ lib/Sub/HandlesVia.pm
@@ -7,7 +7,7 @@ package Sub::HandlesVia;
 use Exporter::Shiny qw( delegations );
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.014';
+our $VERSION   = '0.015';
 
 sub _generate_delegations {
 	my ($me, $name, $args, $globals) = (shift, @_);
--- lib/Sub/HandlesVia/Handler.pm
+++ lib/Sub/HandlesVia/Handler.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::Handler;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.014';
+our $VERSION   = '0.015';
 
 use Class::Tiny (
 	qw(
@@ -388,7 +388,7 @@ sub _generate_handler {
 package Sub::HandlesVia::Handler::Traditional;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.014';
+our $VERSION   = '0.015';
 
 BEGIN { our @ISA = 'Sub::HandlesVia::Handler' };
 
@@ -431,7 +431,7 @@ sub _coderef {
 package Sub::HandlesVia::Handler::CodeRef;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.014';
+our $VERSION   = '0.015';
 
 BEGIN { our @ISA = 'Sub::HandlesVia::Handler' };
 
