abb8fe8f251bc02c1bca6eaef39bbd6ac80b27a9
Toby Inkster <mail@tobyinkster.co.uk>
2022-06-14 16:26:57 +0100

    Documentation for tidied up Sub::HandlesVia::Handler

--- lib/Sub/HandlesVia/Handler.pm
+++ lib/Sub/HandlesVia/Handler.pm
@@ -246,3 +246,250 @@ sub template {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::HandlesVia::Handler - template for a method that can be delegated to
+
+=head1 DESCRIPTION
+
+This module works in conjunction with L<Sub::HandlesVia::CodeGenerator>
+and subclasses of L<Sub::HandlesVia::Toolkit> to build a string of Perl
+code which can be compiled into a method to install into your class.
+
+=head1 CONSTRUCTORS
+
+=head2 C<< new( %attributes ) >>
+
+Standard Moose-like constructor.
+
+=head2 C<< lookup( $method, $trait ) >>
+
+Looks up a method from existing handler libraries.
+
+  my $h = Sub::HandlesVia::Handler->lookup( 'get', 'Array' );
+
+Curried version:
+
+  my $h = Sub::HandlesVia::Handler->lookup( [ 'get', 0 ], 'Array' );
+
+The C<< $trait >> may be an arrayref of possible traits.
+
+=head1 EXPORTS
+
+Nothing is exported by default.
+
+=head2 C<< handler %attributes >>
+
+Shortcut for the C<new> constructor.
+
+  use Sub::HandlesVia::Handler 'handler';
+  
+  my $h = handler( %attr );
+  # is the same as
+  my $h = Sub::HandlesVia::Handler->new( %attr );
+
+=head1 ATTRIBUTES
+
+=head2 C<< name >> B<< Str >>
+
+The name of the function being delegated to.
+
+=head2 C<< is_mutator >> B<Bool>
+
+Indicates whether this handler might mutate an attribute value.
+The default is to try to detect it based on analysis of the templates.
+
+=head2 C<< template >> B<< Str >>
+
+Specially formatted string (see section below) containing the Perl code
+to implement the method.
+
+=head2 C<< lvalue_template >> B<< Maybe[Str] >>
+
+If defined, a shortcut for implementing it when the attribute slot
+value can be used as an lvalue.
+
+=head2 C<< args >> B<< Maybe[PositiveOrZeroInt] >>
+
+The number of arguments which the method being generated expects
+(does not include the attibute value itself).
+
+=head2 C<< min_args >> and C<< max_args >> B<< Maybe[PositiveOrZeroInt] >>
+
+For methods which take a variable number of arguments. If omitted, default
+to C<args>.
+
+=head2 C<< signature >> B<< Maybe[ArrayRef[TypeTiny]] >>
+
+A signature for said arguments.
+
+=head2 C<< usage >> B<< Str >>
+
+A signature to show in documentation, like C<< '$index, $value' >>.
+If not provided, will be generated magically from C<args>, C<min_args>,
+and C<max_args>.
+
+=head2 C<< curried >> B<< Maybe[ArrayRef[Item]] >>
+
+An arrayref of curried arguments.
+
+=head2 C<< is_chainable >> B<Bool>
+
+Whether to force the generated method to be chainable.
+
+=head2 C<< no_validation_needed >> B<Bool>
+
+Whether to do less validation of input data.
+
+=head2 C<< default_for_reset >> B<< Maybe[Str] >>
+
+If this handler has to "reset" an attribute value to its default,
+and the attribute doesn't have a default, this string of Perl code
+is evaluated to provide a default. An example might be C<< "[]" >>.
+
+=head2 C<< documentation >> B<< Maybe[Str] >>
+
+String of pod to describe the handler.
+
+=head2 C<< _examples >> B<< Maybe[CodeRef] >>
+
+This coderef, if called with parameters C<< $class >>, C<< $attr >>, and
+C<< $method >>, will generate a code example to insert into the pod.
+
+=head2 C<< additional_validation >> B<< Maybe[CodeRef] >>
+
+Coderef providing a slightly annoying API. To be described later.
+
+=head2 C<< allow_getter_shortcuts >> B<Bool>
+
+Defaults to true. Rarely useful to override.
+
+=head1 METHODS
+
+=head2 C<< has_min_args() >> and C<< has_max_args() >>
+
+Indicate whether this handler has a defined min or max args.
+
+=head2 C<< install_method( %args ) >>
+
+The required arguments are C<method_name> and C<code_generator>.
+Installs the delegated method into the target class (taken from
+the code generator).
+
+=head2 C<< code_as_string( %args ) >>
+
+Same required arguments as C<install_method>, but returns the Perl
+code for the method as a string.
+
+=head2 C<< curry( @args ) >>
+
+Pseudo-constructor.
+
+Creates a new Sub::HandlesVia::Handler object like this one, but
+with the given arguments curried.
+
+=head2 C<< loose >>
+
+Pseudo-constructor.
+
+Creates a new Sub::HandlesVia::Handler object like this one, but
+with looser argument validation.
+
+=head2 C<< chainable >>
+
+Pseudo-constructor.
+
+Creates a new Sub::HandlesVia::Handler object like this one, but
+chainable.
+
+=head1 TEMPLATE FORMAT
+
+The template is a string of Perl code, except if the following special
+things are found in it, they are substituted.
+
+=over
+
+=item C<< $SELF >>
+
+The invocant.
+
+=item C<< $SLOT >>
+
+Direct hashref access for the attribute.
+
+=item C<< $GET >>
+
+The current value of the attribute.
+
+=item C<< @ARG >>
+
+Any additional arguments passed to the delegated method.
+
+C<< $ARG[$n] >> will also work.
+
+=item C<< #ARG >>
+
+The number of additional arguments passed to the delegated method.
+
+=item C<< $ARG >>
+
+The first element in C<< @ARG >>.
+
+=item C<< $DEFAULT >>
+
+The attribute's default value, if known.
+
+=item C<< « EXPR » >>
+
+An expression in double angled quotes sets the attribute's value to the
+expression.
+
+=back
+
+For example, a handler to halve the value of a numeric attribute might be:
+
+  'Sub::HandlesVia::Handler'->new(
+    name => 'MyNumber:halve',
+    args => 0,
+    template => '« $GET / 2 »',
+    lvalue_template => '$GET /= 2',
+  );
+
+=head1 SUBCLASSES
+
+Sub::HandlesVia::Handler::Traditional and Sub::HandlesVia::Handler::CodeRef
+are provided. See source code for this module for more info.
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/tobyink/p5-sub-handlesvia/issues>.
+
+=head1 SEE ALSO
+
+L<Sub::HandlesVia>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2020, 2022 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
