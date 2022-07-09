b8322c91ad8dd8a38a58d97782b75a516798e431
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-09 19:23:53 +0100

    bump version; changelog

--- lib/Sub/HandlesVia/Toolkit.pm
+++ lib/Sub/HandlesVia/Toolkit.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::Toolkit;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.028';
+our $VERSION   = '0.029';
 
 use Sub::HandlesVia::Mite;
 
--- lib/Sub/HandlesVia/Toolkit/Moo.pm
+++ lib/Sub/HandlesVia/Toolkit/Moo.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::Toolkit::Moo;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.028';
+our $VERSION   = '0.029';
 
 use Sub::HandlesVia::Mite;
 extends 'Sub::HandlesVia::Toolkit';
