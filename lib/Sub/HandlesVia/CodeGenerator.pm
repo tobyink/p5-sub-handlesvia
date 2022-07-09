4c0097def0a23456833f3f3cfebe189498db270d
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-09 19:30:28 +0100

    bump version; changelog

--- lib/Sub/HandlesVia/CodeGenerator.pm
+++ lib/Sub/HandlesVia/CodeGenerator.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::CodeGenerator;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.029';
+our $VERSION   = '0.030';
 
 use Sub::HandlesVia::Mite -all;
 
--- lib/Sub/HandlesVia/Toolkit.pm
+++ lib/Sub/HandlesVia/Toolkit.pm
@@ -5,7 +5,7 @@ use warnings;
 package Sub::HandlesVia::Toolkit;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.029';
+our $VERSION   = '0.030';
 
 use Sub::HandlesVia::Mite;
 
