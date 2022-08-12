1836561292a765a962e4f788c959fd2c2f27d71f
Toby Inkster <mail@tobyinkster.co.uk>
2022-08-12 15:43:24 +0100

    bump version; changelog

--- lib/Sub/HandlesVia.pm
+++ lib/Sub/HandlesVia.pm
@@ -7,7 +7,7 @@ package Sub::HandlesVia;
 use Exporter::Shiny qw( delegations );
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.034';
+our $VERSION   = '0.035';
 
 sub _generate_delegations {
 	my ($me, $name, $args, $globals) = (shift, @_);
