40936313be194425e5ecb8a07f54662773eed5fc
Toby Inkster <mail@tobyinkster.co.uk>
2023-04-05 21:19:59 +0100

    Change versioning scheme

--- lib/Sub/HandlesVia.pm
+++ lib/Sub/HandlesVia.pm
@@ -7,7 +7,7 @@ package Sub::HandlesVia;
 use Exporter::Shiny qw( delegations );
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.046';
+our $VERSION   = '0.050000';
 
 sub _generate_delegations {
 	my ($me, $name, $args, $globals) = (shift, @_);
